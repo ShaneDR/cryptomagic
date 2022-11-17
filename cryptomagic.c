6a56973352aa8f56695e915517bbb657b5b1f2ce
Shane <shane1ramphal@gmail.com>
2022-11-17 17:05:08 -0500

    fixed encryption

--- cryptomagic.c
+++ cryptomagic.c
@@ -1,83 +1,80 @@
 #include <stdio.h>
-#include <string.h> 
-#include <stdlib.h> 
+#include <string.h>
+#include <stdlib.h>
 
-void substring(char* in,char*out, int index, int length){
-int i = 0; 
-while (i < length){
-    out[i] = in[index+i];
+void substring(char *in, char *out, int index, int length)
+{
+  int i = 0;
+  while (i < length)
+  {
+    out[i] = in[index + i];
     i++;
-}
-out[length] = '\0';
-}
-
-
-void encrypt(char *fname){
-char *pptr = strchr(fname, '.');
-int ppos = pptr - fname; 
-char newName[ppos+1];
-substring(fname, newName, 0, ppos);
-strcat(newName, ".crp");
-
-char outChar; 
-char end = NULL;
-FILE *in = fopen(fname, "r"); 
-if (in == NULL){
-  puts("Error: Empty File");
-}
-FILE *out = fopen(newName, "w");
-if (out == NULL){
-  puts("Error: Empty File");
-}
-int run = 1; 
-while(run == 1);  {
-  outChar = fgetc(in); 
-  printf("%c", outChar);
-
-  if (outChar == '\0'){
-    run = 0; 
   }
-  else if (outChar == '\n'){
-    fputc('\n', out);
+  out[length] = '\0';
+}
+
+void encrypt(char *fname)
+{
+  char *pptr = strchr(fname, '.');
+  int ppos = pptr - fname;
+  char newName[ppos + 1];
+  substring(fname, newName, 0, ppos);
+  strcat(newName, ".crp");
+
+  FILE *in = fopen(fname, "r");
+  if (in == NULL)
+  {
+    puts("Error: Empty File");
   }
-  else if(outChar == '\t'){
-    fputc('\t', out);
- 
+
+  FILE *out = fopen(newName, "w");
+  if (out == NULL)
+  {
+    puts("Error: Empty File");
   }
-  else {
-    outChar = outChar - 16; 
-     if (outChar < 32){
-     outChar =(outChar - 32) + 144;
-     fprintf(out,"%02X",outChar);
+
+  char outChar;
+  while (outChar != EOF)
+    {
+        if (outChar == '\n')
+        {
+            fputc('\n', out);
+        }
+        else if (outChar == '\t')
+        {
+            fputs("TT", out);
+        }
+        else
+        {
+            outChar = outChar - 16;
+            if (outChar < 32)
+            { 
+                outChar = (outChar - 32) + 144;
+            }
+            fprintf(out, "%02X", (int)(*(unsigned char*)(&outChar)));
+        }
+        outChar = fgetc(in);
+
     }
-  }
-
-
 }
 
+int main(int argc, char *argv[])
+{ // argc = number of parameters passed, argv = pointer to array of parameters passed
 
-
-
-
-}
-
-
-
-int main (int argc, char *argv[]){ // argc = number of parameters passed, argv = pointer to array of parameters passed 
-
-  if (argc == 1) { // if the program is ran and no switch is passed, execute the code below
-    printf("Enter cryptoMagic -E \"File Name\".txt to encript a file\nEnter cryptoMagic -D \"File Name\".crp to decrypt a file");   
+  if (argc == 1)
+  { // if the program is ran and no switch is passed, execute the code below
+    printf("Enter cryptoMagic -E \"File Name\".txt to encript a file\nEnter cryptoMagic -D \"File Name\".crp to decrypt a file");
   }
 
-  if (argc == 3){ // if the user passes 3 parameters, execute code below
-    if (strcmp(argv[1], "-E") == 0){ // if the inputted string is "-E", encrypt the specified file
-    encrypt(argv[2]);
+  if (argc == 3)
+  { // if the user passes 3 parameters, execute code below
+    if (strcmp(argv[1], "-E") == 0)
+    { // if the inputted string is "-E", encrypt the specified file
+      encrypt(argv[2]);
     }
 
-    
-    else if (strcmp(argv[1], "-D") == 0){ // if the inputted string is "-D", decrypt the specified file
-    
-
-   }
+    else if (strcmp(argv[1], "-D") == 0)
+    { // if the inputted string is "-D", decrypt the specified file
+    }
   }
 }
--- test.c
+++ test.c
@@ -1,57 +1,53 @@
 #include <stdio.h>
-#include <string.h> 
+#include <string.h>
 #include <stdlib.h>
 
-void substring(char* in,char*out, int index, int length){
-int i = 0; 
-while (i < length){
-    out[i] = in[index+i];
-    i++;
+void substring(char *in, char *out, int index, int length)
+{
+    int i = 0;
+    while (i < length)
+    {
+        out[i] = in[index + i];
+        i++;
+    }
+    out[length] = '\0';
 }
-out[length] = '\0';
-}
-int main (){
-char c[] = "text.txt";
-char *pptr = strchr(c, '.');
-int ppos = pptr - c; 
-char newName[ppos+1];
-substring(c, newName, 0, ppos);
-strcat(newName, ".crp");
+int main()
+{
+    char c[] = "text.txt";
+    char *pptr = strchr(c, '.');
+    int ppos = pptr - c;
+    char newName[ppos + 1];
+    substring(c, newName, 0, ppos);
+    strcat(newName, ".crp");
 
-char ch; 
-FILE *in = fopen(c, "r"); 
-FILE *out = fopen(newName, "w+");
+    char ch;
+    FILE *in = fopen(c, "r");
+    FILE *out = fopen(newName, "w+");
 
-int run = 1;
-ch = fgetc(in);
-while (ch != EOF){
-  
-        if (ch == '\n'){
+    int run = 1;
+    ch = fgetc(in);
+    while (ch != EOF)
+    {
+        if (ch == '\n')
+        {
             fputc('\n', out);
-     }
-        else if(ch == '\t'){
-        fputs("TT", out);
-     }
-        else {
-            ch = ch - 16; 
-             if (ch < 32){
-            ch =(ch - 32) + 144;
-            printf("%c", ch);
-            fprintf(out,"%02X",ch);
-            } else {
-            fprintf(out,"%02X",ch);
-
+        }
+        else if (ch == '\t')
+        {
+            fputs("TT", out);
+        }
+        else
+        {
+            ch = ch - 16;
+            if (ch < 32)
+            { 
+                ch = (ch - 32) + 144;
             }
-        
-       }  
+            fprintf(out, "%02X", (int)(*(unsigned char*)(&ch)));
+        }
         ch = fgetc(in);
 
     }
+    printf("\nworked");
 }
-
-
-
-
-
-
-
